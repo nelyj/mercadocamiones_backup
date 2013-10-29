edeaf18f3de93ec217b0066c9f5e94586a9482ac
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-10-29 15:41:21 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mercadocamiones::Application.config.secret_token = '9def6ad766412dc14e73cc63d4a2584cd0210c75740c124009b139e3c7cf52801641051d79ae604598ce5ea4b697576d02080d8dd9840d31bcc87612eaa03533'
