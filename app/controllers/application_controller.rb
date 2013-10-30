4efa15e21dcf7b2e75282ac2060a135f3f1307d4
Nelson Pato Jiménez <developer.jimenez@gmail.com>
2013-10-29 22:02:57 -0300

    Se crea el widget categoria y el widget social, ademas se agrega un redireccionamiento de no-www a www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :add_www_subdomain
+
+  private
+  def add_www_subdomain
+    unless /^www/.match(request.host)
+      redirect_to("#{request.protocol}x.com#{request.request_uri}",
+                  :status => 301)
+    end
+  end
 end
